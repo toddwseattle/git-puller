eef02c56f0ec5aa8801e4fdd7d13956d2188cf9e
Todd Warren <todd@warrenfamily.org>
2017-11-02 15:00:23 -0700

    Create Initial CLI app and Firebase Initial
    Adjust .gitignore to ignore the funcitons node modules component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
